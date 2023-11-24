3d97dafe1ef180187baf3948edf581c45007eaa0
pnr3 <149761666+pnr3@users.noreply.github.com>
2023-11-24 14:30:48 +0900

    Add files via upload

---
+++ hw12/5/1124.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+#include "Car.h"
+#include "SUV3.h"
+#include "Engineer.h"
+int main(int argc, char*argv[]) {
+	Car* newSUV = new SUV(); 
+	cout << " with virtual \n";
+	newSUV->klaxon(3);
+
+	cout << "\n with virtual \n";
+	newSUV->sayMyName();
+
+	delete newSUV;
+	return 0;
+}
---
+++ hw12/5/SUVS3.cpp
@@ -0,0 +1,19 @@
+#include "SUV3.h"
+#include <iostream>
+void SUV::setSparewheels(int ii) {
+	spareWheels = ii;
+}
+int SUV::getSparewheels() {
+	return spareWheels;
+};
+float SUV::getTotalPrice() {
+	return (this->wheels * 100.0 + this->price);
+}
+void SUV::klaxon(int ii) {
+	for (int i = 0; i < ii; i++) {
+		std::cout << "Beep\n";
+	}
+}
+void SUV::sayMyName() {
+	std::cout << " my name is SUV. \n";
+}
